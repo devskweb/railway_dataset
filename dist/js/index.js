901c412e5d72757ee97d527bde74a2a0836c6672
vikas <santoshkumar1561997@gmail.com>
2019-01-26 22:53:23 +0530

    'next'

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,24 +1,15 @@
 const app = document.getElementById("root");
 
-const logo = document.createElement("img");
-logo.src = "./img/logo.png";
+//var input = document.createElement("input");
+//input.setAttribute("type", "text");
 
 const container = document.createElement("div");
 container.setAttribute("class", "container");
 
-app.appendChild(logo);
-app.appendChild(container);
-/*fetch("data.json")
-  .then(res => res.json())
-  .then(data => {
-    let root = "<h2>Train Number</h2>";
-    data.forEach(function(post) {
-      root += ' <div class="card"><h1>'
-      ${post.Train_number}</h1>';
-    });
-  });
+//app.appendChild(input);
 
-*/
+app.appendChild(container);
+
 var request = new XMLHttpRequest();
 request.open("GET", "data.json", true);
 request.onload = function() {
@@ -44,31 +35,36 @@ request.onload = function() {
     card.appendChild(h3);
     card.appendChild(h4);
   }
-  // Begin accessing JSON data here
-  /*var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(train => {
-      
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h2 = document.createElement("h2");
-      h1.textContent = train.train_number;
-
-      const h1 = document.createElement("h1");
-      train.train_number = movie.train_name;
-      p.textContent = `${train.train_name}...`;
-
-      container.appendChild(card);
-      card.appendChild(h2);
-      card.appendChild(h1);
-    });
-  } else {
-    const errorMessage = document.createElement("marquee");
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-};
-*/
 };
 request.send();
+$(document).ready(function() {
+  $("#search").keydown(function(data) {
+    $("#result").html("");
+    var searchField = $("#search").val();
+    var expression = new RegExp(searchField, "i");
+    $.getJSON("data.json", function(data) {
+      $.each(data, function(key, value) {
+        if (
+          value.Train_name.search(expression) != -1 ||
+          value.Train_no.search(expression) != -1 ||
+          value.location.search(expression) != -1
+        ) {
+          $("#result").append(
+            '<li class="list-group-item"><h3>' +
+              value.Train_no +
+              'height="40" width="40" class="img-thumbnail"/>' +
+              value.Train_name +
+              '|<span class="text-muted"> origin:' +
+              value.Starts +
+              '</span>|<span class="text-muted"> destination:' +
+              value.Ends +
+              "</span></li>"
+          );
+        }
+      });
+    });
+  });
+});
+//var input;
+//input = document.getElementById("myInput").value;
+//console.log(input);
